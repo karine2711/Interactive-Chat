5c17d6a8ad65765d72684b08aa8ef8ca8d01637f
Karine <karine.gevorgyan.2001@mail.ru>
2019-12-22 18:44:23 +0400

    Commented out the calls of getmessages and printChat mthods. Replaced the code that added members' names to the array from Main.java to Chatting.java. Also changed the message for adding members

--- src/Chatting.java
+++ src/Chatting.java
@@ -1,27 +1,35 @@
 import java.time.LocalDateTime;
-
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+
 public class Chatting {
-    public static void getmessages( Member member){
-        Scanner scanner=new Scanner(System.in);
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
-
-
-            System.out.println("Hi "+member.getFullName()+". Do you want to input message? 1 for 'Yes' and 0 for 'No'");
-            int order = scanner.nextInt();
-            if(order==1) {
-                member.setHasMessage(true);
-                System.out.print(member.getFullName() + ":  ");
-                scanner.nextLine();
-                member.setMessage(scanner.next());
-                member.date = (dtf.format(LocalDateTime.now()) + "    ");
-            }
+  private static Scanner scanner = new Scanner(System.in);
+    public static void getMemberList(int number, Member members[]){
+        for (int i = 0; i < number; i++) {
+            System.out.print("Enter the name of member"+(i+1)+":  ");
+            members[i]= new Member(scanner.next());
+           // Chatting.getmessages(members[i]);
         }
-public static void printChat(Member member){
-    if (member.HasMessage()) {
-        System.out.println();
-        System.out.print(member.getDate() + "  " + member.getFullName() + "  " + member.getMessage());
     }
-}
+
+    public static void getmessages(Member member) {
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+        System.out.println("Hi " + member.getFullName() + ". Do you want to input message? 1 for 'Yes' and 0 for 'No'");
+        int order = scanner.nextInt();
+        if (order == 1) {
+            member.setHasMessage(true);
+            System.out.print(member.getFullName() + ":  ");
+            scanner.nextLine();
+            member.setMessage(scanner.next());
+            member.date = (dtf.format(LocalDateTime.now()) + "    ");
+        }
+    }
+
+    public static void printChat(Member member) {
+        if (member.HasMessage()) {
+            System.out.println();
+            System.out.print(member.getDate() + "  " + member.getFullName() + "  " + member.getMessage());
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,12 @@ public class Main {
         Member[] members = new Member[10];
         int number = 0;
 
-        System.out.println("Please enter the number of members");
+        System.out.print("Please enter the number of members    ");
         number = scanner.nextInt();
-        for (int i = 0; i < number; i++) {
-            System.out.print("Enter your name  ");
-            members[i]= new Member(scanner.next());
-            Chatting.getmessages(members[i]);
-        }
-            for (int i = 0; i < number; i++) {
-                Chatting.printChat(members[i]);
-            }
+Chatting.getMemberList(number, members);
+          //  for (int i = 0; i < number; i++) {
+           //     Chatting.printChat(members[i]);
+          //  }
 
 
 
