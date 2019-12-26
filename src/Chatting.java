a07dd4434ba2055bff65ad2b7a579515fd12c3d7
Karine <karine.gevorgyan.2001@mail.ru>
2019-12-26 19:57:43 +0400

    Replaced the public static void main(..) from Main.java to Chatting.java. Changed the Chatting  method calls from Chatting.methodName to methodName.

--- src/Chatting.java
+++ src/Chatting.java
@@ -6,9 +6,11 @@ import java.util.Random;
 public class Chatting {
     private static Scanner scanner = new Scanner(System.in);
     private static ArrayList indexesofActiveMembers = new ArrayList();
+
     public static ArrayList getIndexesofActiveMembers() {
         return indexesofActiveMembers;
     }
+
     public static void initMembers(int number, Member[] members) {
         for (int i = 0; i < number; i++) {
             System.out.print("Enter the name of member" + (i + 1) + ":  ");
@@ -22,13 +24,45 @@ public class Chatting {
         return rand.nextInt(indexesofActiveMembers.size());
     }
 
-
-
     public static void askForOrder(Member member) {
         System.out.println(member.getFullName() + String.valueOf(Character.toChars(0x1F495)));
         System.out.print("Your order(1 or 2):  ");
     }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number of members. The maximum number is 30.");
+        System.out.print("Number of members:  ");
+        int number = scanner.nextInt();
+        Member[] members = new Member[number];
+        initMembers(number, members);
+        for (int i = 0; i < number; i++)
+          indexesofActiveMembers.add(i);
+        System.out.println("Dear members please enter 1 if you wanna continue chatting and 2 if you wanna exit");
+
+        while (!indexesofActiveMembers.isEmpty()) {
+            int index = selectRandomMemberIndex();
+            int currentIndex = (int) indexesofActiveMembers.get(index);
+            askForOrder(members[currentIndex]);
+            String order = scanner.next();
+            int attempts = 0;
+            while (!order.equals("1") && !order.equals("2")) {
+                if (attempts > 2) {
+                    Member.kickOut(members[currentIndex], index);
+                    break;
+                }
+                attempts++;
+                System.out.println("This is your attempt   " + attempts + " / 3!   After that, you'll be kicked out.   ");
+                System.out.print("Your order(1 or 2):  ");
+                order = scanner.next();
+            }
+            if (order.equals("2")) {
+                Member.setMemberOffline(index);
+            } else if (order.equals("1")) {
+                members[currentIndex].sendMessage();
+            }
+        }
+    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,5 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the number of members. The maximum number is 30.");
-        System.out.print("Number of members:  ");
-        int number = scanner.nextInt();
-        Member[] members = new Member[number];
-        Chatting.initMembers(number, members);
-        for (int i = 0; i < number; i++)
-            Chatting.getIndexesofActiveMembers().add(i);
-        System.out.println("Dear members please enter 1 if you wanna continue chatting and 2 if you wanna exit");
 
-        while (!Chatting.getIndexesofActiveMembers().isEmpty()) {
-            int index = Chatting.selectRandomMemberIndex();
-            int currentIndex = (int) Chatting.getIndexesofActiveMembers().get(index);
-            Chatting.askForOrder(members[currentIndex]);
-            String order = scanner.next();
-            int attempts = 0;
-            while (!order.equals("1") && !order.equals("2")) {
-                if (attempts > 2) {
-                    Member.kickOut(members[currentIndex], index);
-                    break;
-                }
-                attempts++;
-                System.out.println("This is your attempt   " + attempts + " / 3!   After that, you'll be kicked out.   ");
-                System.out.print("Your order(1 or 2):  ");
-                order = scanner.next();
-            }
-            if (order.equals("2")) {
-                Member.setMemberOffline(index);
-            } else if (order.equals("1")) {
-                members[currentIndex].sendMessage();
-            }
-        }
-    }
 }
